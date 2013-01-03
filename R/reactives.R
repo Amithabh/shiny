0627ea0bfa921a47f88cca60a53937d781327742
Joe Cheng <joe@rstudio.org>
2013-01-02 16:23:18 -0800

    Add non-reactive scope impl

--- R/reactives.R
+++ R/reactives.R
@@ -392,3 +392,20 @@ invalidateLater <- function(millis) {
   })
   invisible()
 }
+
+#' Create a non-reactive scope for an expression
+#'
+#' Takes an expression and creates a new context for evaluating that expression
+#' which can receive invalidations but does not in turn invalidate other
+#' contexts. This is useful when you want a function to access reactive values
+#' or functions but don't want changes to those values to trigger re-evaluation
+#' of the function.
+#'
+#' @param expr An expression that can access reactive values or functions.
+#' @export
+nonreactive <- function(expr) {
+  ctx <- Context$new()
+  ctx$run(function() {
+    eval.parent(expr)
+  })
+}
